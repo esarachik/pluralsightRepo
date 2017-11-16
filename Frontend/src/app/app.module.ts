8b1fdae86004c3f1ad2eeb925ec95beef83101c5
elias.sarachik <elias.sarachik@globant.com>
2017-11-15 23:41:33 -0300

    Frontend

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import {MatButtonModule , MatCardModule} from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages.component'
+import { ApiService } from './api.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent, MessagesComponent
+  ],
+  imports: [
+    BrowserModule, HttpModule, MatButtonModule, MatCardModule
+  ],
+  providers: [ApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
